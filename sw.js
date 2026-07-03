d6708c790db7ec5edf75dc26dda118772c429cbd
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-03 10:20:27 +0200

    Add greenhouse floor span numbering guidance

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-siechnice-modular-";
-const CACHE_NAME = CACHE_PREFIX + "2026-07-03-07";
+const CACHE_NAME = CACHE_PREFIX + "2026-07-03-08";
 
 const CORE_ASSETS = [
   "./",
