cab0162d5e6a35782b72eda20f9de154a8224084
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-13 22:37:01 +0200

    Collapse medical cards on mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-siechnice-modular-";
-const CACHE_NAME = CACHE_PREFIX + "20260713-citycollapse1-siechnice";
+const CACHE_NAME = CACHE_PREFIX + "20260713-medaccordion2-siechnice";
 
 const CORE_ASSETS = [
   "./",
@@ -18,9 +18,9 @@ const CORE_ASSETS = [
   "./zakazy.html",
   "./test.html",
   "./manifest.webmanifest",
-  "./assets/css/training.css?v=20260713-citycollapse1-siechnice",
-  "./assets/js/training-data.js?v=20260713-citycollapse1-siechnice",
-  "./assets/js/training-app.js?v=20260713-citycollapse1-siechnice",
+  "./assets/css/training.css?v=20260713-medaccordion2-siechnice",
+  "./assets/js/training-data.js?v=20260713-medaccordion2-siechnice",
+  "./assets/js/training-app.js?v=20260713-medaccordion2-siechnice",
   "./assets/logo-citronex.svg"
 ];
 
