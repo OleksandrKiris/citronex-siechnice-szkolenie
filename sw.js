07cafdc9ad355d2340566a99dcad1c80a1be3773
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-15 12:35:53 +0200

    Improve frontend visual clarity

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-siechnice-modular-";
-const CACHE_NAME = CACHE_PREFIX + "20260715-frontend-comfort3-siechnice";
+const CACHE_NAME = CACHE_PREFIX + "20260715-frontend-clarity4-siechnice";
 
 const CORE_ASSETS = [
   "./",
@@ -18,9 +18,9 @@ const CORE_ASSETS = [
   "./zakazy.html",
   "./test.html",
   "./manifest.webmanifest",
-  "./assets/css/training.css?v=20260715-frontend-comfort3-siechnice",
-  "./assets/js/training-data.js?v=20260715-frontend-comfort3-siechnice",
-  "./assets/js/training-app.js?v=20260715-frontend-comfort3-siechnice",
+  "./assets/css/training.css?v=20260715-frontend-clarity4-siechnice",
+  "./assets/js/training-data.js?v=20260715-frontend-clarity4-siechnice",
+  "./assets/js/training-app.js?v=20260715-frontend-clarity4-siechnice",
   "./assets/logo-citronex.svg"
 ];
 
